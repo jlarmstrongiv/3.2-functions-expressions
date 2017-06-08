2cfc2596f9d1b074ccbeb3a49b988cb4239f757f
John Armstrong <jlarmst@g.clemson.edu>
2017-06-08 16:00:00 -0400

    simplfied number two

--- main.js
+++ main.js
@@ -16,19 +16,7 @@ console.log(max(1, 2));
 // A:
 
 function maxOfThree (num1, num2, num3) {
-  if (num1 > num2) {
-    if (num1 > num3) {
-      return num1;
-    } else {
-      return num3;
-    }
-  } else {
-    if (num2 > num3) {
-      return num2;
-    } else {
-      return num3;
-    }
-  }
+  return Math.max(num1, num2, num3);
 }
 console.log(maxOfThree(1, 2, 3));
 
